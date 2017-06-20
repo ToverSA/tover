b2c1932779a288cbb51de384651ef33d72df7b33
sduduzog <gumedesduduzo@gmail.com>
2017-06-20 02:34:53 +0200

    added code for GET users

--- _php/users.php
+++ _php/users.php
@@ -7,6 +7,7 @@ class Users{
   //TODO Send email verification
   public static function postNewUser(){
     $con = new mysqli(HOST, USER, PWD, DB);
+    $con->autocommit(false);
     $con->query('START TRANSACTION');
     $query = 'INSERT INTO login (email, password, token) VALUES (?, ?, ?)';
     $stmt = $con->prepare($query);
@@ -32,7 +33,6 @@ class Users{
       header('HTTP/1.0 400 Bad request');
     }
     $con->query('COMMIT');
-    //TODO sql transaction testing
   }
   public static function postAuthUser(){
     $con = new mysqli(HOST, USER, PWD, DB);
@@ -75,6 +75,36 @@ class Users{
       }
     } else {
       header('HTTP/1.0 403 Forbidden');
+    }
+  }
+  public static function getUsers(){
+    if (isset($_GET['id'])){
+      echo 'one user';
+    } else if (isset($_GET['cid']) && isset($_GET['q'])){
+      $con = new mysqli(HOST, USER, PWD, DB);
+      $query = 'SELECT login.id, login.email, users.name, users.number, COUNT(advert.id) ';
+      $query = $query . 'FROM login JOIN users JOIN advert ';
+      $query = $query . 'ON login.id=users.id AND login.id=advert.user_id ';
+      $query = $query . "WHERE users.campus_id=? AND users.name LIKE '%".$_GET['q']."%' ";
+      $query = $query . 'GROUP BY login.id, users.name, users.number';
+      $stmt = $con->prepare($query);
+      // print_r($con);
+      $stmt->bind_param('i', $_GET['cid']);
+      $stmt->execute();
+      $stmt->bind_result($id, $email, $name, $num, $num_ads);
+      $arr = array();
+      while ($stmt->fetch()){
+        $u = new Users();
+        $u->id = $id;
+        $u->name = $name;
+        $u->email = $email;
+        $u->number = $num;
+        $u->num_ads = $num_ads;
+        array_push($arr, $u);
+      }
+      echo json_encode($arr);
+    } else {
+      echo 'all users';
     }
   }
 }
