67e217fdd0120b56b9a64cb172aac19cd89b5b56
sduduzog <gumedesduduzo@gmail.com>
2018-04-16 22:45:34 +0200

    Started item view

--- app/market/components/MarketItemView.vue
+++ app/market/components/MarketItemView.vue
@@ -1,10 +1,15 @@
 <template lang="html">
-<div class="market-item-wrapper">
-  <div class="main-content">
+<div class="">
+  <div class="loading"></div>
+  <div class="error"></div>
+  <div class="market-item-wrapper">
+    <div class="main-content">
+      <img src="../../../assets/images/placeholder.jpg" alt="">
+      <h3 class="title">Lorem ipsum dolor sit amet.</h3>
+    </div>
+    <div class="side-content">
 
-  </div>
-  <div class="side-content">
-
+    </div>
   </div>
 </div>
 </template>
@@ -14,12 +19,13 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '../../app.scss';
 .market-item-wrapper{
   display: grid;
   grid-template-columns: 700px 1fr;
   padding-left: 100px;
+  margin-top: 5px;
 
   @media screen and (max-width: 1024px){
     grid-template-columns: 500px 1fr;
@@ -36,10 +42,54 @@ export default {
   .main-content{
     background-color: #FFF;
     padding: 10px;
+    display: grid;
+    grid-template-columns: 250px 1fr 1fr;
+
+    @media screen and (max-width: 1024px){
+      grid-template-columns: 150px 1fr 1fr
+    }
+
+    @media screen and (max-width: 425px){
+      grid-template-columns: 100px 1fr 1fr
+    }
+
+    img{
+      width: 100%;
+      height: auto;
+    }
+
+    h3.title{
+      grid-column: 2 / 4;
+      padding: 10px;
+      margin: 0;
+      overflow: hidden;
+      position: relative;
+      height: 8.4em;
+      font-size: 1.6em;
+
+      @media screen and (max-width: 1024px){
+        font-size: 1.17em;
+      }
+      @media screen and (max-width: 425px){
+        padding: 0;
+        padding-left: 10px;
+        font-size: 1em;
+        height: 5.3em;
+      }
+    }
+    h3.title:after {
+      content: "";
+      text-align: right;
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      width: 70%;
+      height: 1.2em;
+      background: linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 50%);
+    }
   }
 
   .side-content{
-    background-color: red;
     height: 20px;
   }
 }
--- app/market/components/MarketView.vue
+++ app/market/components/MarketView.vue
@@ -4,7 +4,12 @@
     <!-- Put some header items here -->
   </div>
   <div class="p-s">
-    <div class="market-item" @click="test(i)" v-for="i in [1, 2, 3, 4, 5, 6, 7]">{{i}}</div>
+    <div class="market-item" @click="test(i)" v-for="i in [1, 2, 3, 4, 5, 6]">
+      <img src="../../../assets/images/placeholder.jpg" alt="Placeholder">
+      <p class="title">Lorem ipsum dolor sgt amet, consectetur adipisicing elit. Nulla, dolorum?</p>
+      <h2 class="price">R200</h2>
+      <span>09/06/18</span>
+    </div>
   </div>
 </div>
 </template>
@@ -24,7 +29,7 @@ export default {
 @import '../../app.scss';
 
 .market-wrapper{
-  
+
   .market-header{
     height: $header-height;
   }
@@ -56,8 +61,11 @@ export default {
     .market-item{
       background-color: #FFF;
       margin: 5px;
-      height: 100px;
       width: 170px;
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      padding: 5px;
+      cursor: pointer;
 
       @media screen and (max-width: 768px){
         width: 125px;
@@ -65,6 +73,39 @@ export default {
 
       @media screen and (max-width: 425px){
         width: 95px;
+      }
+
+      img{
+        width: 100%;
+        height: auto;
+        grid-column: 1 / 3;
+      }
+
+      p.title{
+        grid-column: 1 / 3;
+        overflow: hidden;
+        position: relative;
+        height: 2.8em;
+        margin: 5px 0;
+      }
+      p.title:after {
+        content: "";
+        text-align: right;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        width: 50%;
+        height: 1.2em;
+        background: linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 50%);
+      }
+
+      h2.price{
+        margin: 0;
+      }
+      span{
+        opacity: 0.5;
+        align-self: end;
+        text-align: right;
       }
     }
   }
