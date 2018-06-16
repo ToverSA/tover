ef7f31d1242608270c7d978503c8296eecc32772
sduduzog <gumedesduduzo@gmail.com>
2018-06-16 14:03:39 +0200

    Renamed dir

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,4 @@
 import { routes as account } from './account';
-import { routes as market } from './market';
+import { routes as store } from './store';
 
-export default [ ...account, ...market ];
+export default [ ...account, ...store ];
