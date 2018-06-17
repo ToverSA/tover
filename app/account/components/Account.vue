6b60e8b9e7d616897ae0d5d2975f87dd90ba385d
sduduzog <gumedesduduzo@gmail.com>
2018-06-17 12:20:51 +0200

    Account fix init

--- app/account/components/Account.vue
+++ app/account/components/Account.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="">
-
+  <div class="account">
+    <div class="sidebar">
+      hello
+    </div>
+    <div class="main"></div>
   </div>
 </template>
 
 <script>
-import AccountHeader from './AccountHeader'
 import store from '../../../store'
 export default {
-  components: { AccountHeader }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '../../app.scss';
+div.account{
+  display: flex;
+
+  .sidebar{
+    flex-basis: 320px;
+    background-color: $primary-color;
+    @media screen and (max-width: 768px){
+      flex-basis: 240px;
+    }
+  }
+  .main{
+    flex-grow: 1;
+    height: 100vh;
+    overflow: auto;
+  }
+}
+</style>
--- app/account/components/index.js
+++ app/account/components/index.js
@@ -1,3 +1,2 @@
 export { default as Account } from './Account';
-export { default as AccountHeader } from './AccountHeader';
 export { default as Auth } from './Auth';
