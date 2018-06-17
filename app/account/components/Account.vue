8d6867bf40483224cda3ad35d15e09c394957eb4
sduduzog <gumedesduduzo@gmail.com>
2018-06-17 13:16:40 +0200

    Major designs

--- app/account/components/Account.vue
+++ app/account/components/Account.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="account">
     <div class="sidebar">
-      hello
+      <div class="header">
+        <img src="../../../assets/images/tover_logo.png" alt="Tover logo">
+        <h3>gumedesduduzo@gmail.com</h3>
+      </div>
+      <div class="list-item">
+        <i class="material-icons">add</i>
+        <span>Create new</span>
+      </div>
+      <div class="list-item">
+        <i class="material-icons">collections_bookmark</i>
+        <span>Adverts</span>
+      </div>
+      <div class="list-item">
+        <i class="material-icons">person_outline</i>
+        <span>Profile</span>
+      </div>
+      <div class="list-item">
+        <i class="material-icons">chat_bubble_outline</i>
+        <span>Chat</span>
+      </div>
+      <div class="list-item">
+        <i class="material-icons">exit_to_app</i>
+        <span>Log out</span>
+      </div>
     </div>
     <div class="main"></div>
   </div>
@@ -19,10 +42,50 @@ div.account{
   display: flex;
 
   .sidebar{
-    flex-basis: 320px;
+    width: 320px;
     background-color: $primary-color;
     @media screen and (max-width: 768px){
-      flex-basis: 240px;
+      width: 240px;
+    }
+
+    .header{
+      display: grid;
+      grid-template-columns: 70px 1fr;
+      padding: 20px 10px;
+      padding-right: 0;
+      background-color: $accent-color;
+
+      img{
+        width: 100%;
+        display: block;
+      }
+      h3{
+        margin: 0;
+        color: $primary-color-text;
+        font-family: Lato Bold;
+        padding: 20px;
+        padding-right: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+    .list-item{
+      // background-color: red;
+      margin: 10px 10px 0 50px;
+      color: $primary-color-text;
+      display: grid;
+      grid-template-columns: 24px 1fr;
+      padding: 10px;
+      grid-gap: 10px;
+      cursor: pointer;
+      &:hover{
+        background-color: $primary-color-dark;
+      }
+
+      span{
+        // background-color: green;
+        padding: 2px;
+      }
     }
   }
   .main{
