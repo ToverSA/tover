caf26e7a2b2bb7ee78e5b39ac34f4183152bef68
sduduzog <gumedesduduzo@gmail.com>
2018-06-29 13:54:01 +0200

    Another prayer

--- app/home/components/Market.vue
+++ app/home/components/Market.vue
@@ -2,10 +2,10 @@
   <div class="store">
     <action-bar/>
     <div class="content">
-      <div class="box" v-for="i in [1, 2, 3, 4, 5]" @click="goto(i)">
+      <div class="box" v-for="i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]" @click="goto(i)">
         <img src="../../../assets/images/albumart.jpg">
         <h3>R123 456</h3>
-        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse nostrum id sapiente cupiditate perspiciatis quasi!</span>
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, excepturi.</span>
       </div>
     </div>
   </div>
@@ -30,14 +30,20 @@ export default {
 @import "../../app.scss";
 div.store {
   div.content {
-    padding: 10px;
+    padding-top: 10px;
     margin: auto;
-    width: 90%;
+    width: 100%;
+    max-width: 1200px;
     display: grid;
     grid-template-columns: repeat(5, 1fr);
-    grid-gap: 10px;
+    align-items: center;
+    grid-row-gap: 10px;
     @media screen and (max-width: 1024px) {
       grid-template-columns: repeat(4, 1fr);
+      max-width: 900px;
+    }
+    @media screen and (max-width: 768px) {
+      max-width: 650px;
     }
     @media screen and (max-width: 450px) {
       grid-template-columns: repeat(3, 1fr);
@@ -46,11 +52,29 @@ div.store {
 
     .box {
       background-color: #fff;
-      padding: 10px;
       cursor: pointer;
+      margin: auto;
+      padding: 20px;
+      width: 200px;
       @include no-select();
+      @media screen and (max-width: 1024px) {
+        width: 200px;
+      }
+      @media screen and (max-width: 768px) {
+        padding: 10px;
+        width: 150px;
+      }
       @media screen and (max-width: 450px) {
+        padding: 10px;
+        width: 140px;
+      }
+      @media screen and (max-width: 425px) {
+        width: 120px;
         padding: 5px;
+      }
+      @media screen and (max-width: 375px) {
+        padding: 5px;
+        width: 100px;
       }
 
       img {
