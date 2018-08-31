bf28383e6e84f98f394856640bde66c4ca2551f8
sduduzog <gumedesduduzo@gmail.com>
2018-08-31 16:43:04 +0200

    Added comments and cleanup

--- src/api.ts
+++ src/api.ts
@@ -4,11 +4,23 @@ import MockAdapter from 'axios-mock-adapter';
 const mock = new MockAdapter(axios, { delayResponse: 500 });
 
 mock.onGet('/users').reply(200);
+mock.onPost('/api/users').reply(201, {
+  message: 'Created successfully',
+});
+
+if (process.env.NODE_ENV === 'production') {
+  mock.restore();
+}
 
 export default {
-  getUsers() {
-    axios.get('users').then((response) => {
-      console.log(response);
-    });
+  /**
+   * Creates user sending a POST /api/users to server endpoint
+   * @param string names of the user to create
+   * @param string email of user
+   * @param string password
+   * @returns AxiosPromise
+   */
+  createUser(names: string, email: string, password: string): AxiosPromise {
+    return axios.post('/api/users', { names, email, password });
   },
-}
+};
