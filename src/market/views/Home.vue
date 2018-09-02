825d5c1fb2a732800580e2d88778e90e44d380ed
sduduzog <gumedesduduzo@gmail.com>
2018-09-02 20:29:57 +0200

    card ratio

--- src/market/views/Home.vue
+++ src/market/views/Home.vue
@@ -17,11 +17,18 @@
       <div class="section" v-bind:key="i" v-for="i in [1, 2]">
         <div class="wrapper">
           <div class="heading">
-          <h2>Section {{i}}</h2>
-          <button class="btn-rounded btn-accent-alpha">
-            <span>VIEW MORE</span>
-          </button>
-        </div>
+            <h2>Section {{i}}</h2>
+            <button class="btn-rounded btn-accent-alpha">
+              <span>MORE</span>
+            </button>
+          </div>
+          <div class="card-wrapper">
+            <div v-bind:key="j" v-for="j in [1, 2, 3, 4, 5, 6, 7, 8]" class="card">
+              <div class="inner">
+                <div class="outer"></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <!-- <app-footer/> -->
@@ -123,14 +130,55 @@ div.home {
         padding-right: 5px;
         h2 {
           margin: 0;
-          font-weight: lighter;
           padding: 5px;
           color: rgb(112, 112, 112);
+          font-weight: normal;
+        }
+      }
+      .card-wrapper {
+        padding: 5px;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-gap: 20px;
+        .card {
+          background-color: rgb(236, 233, 233);
+          padding: 5px;
+          .outer {
+            position: relative;
+            &:before {
+              display: block;
+              content: '';
+              width: 100%;
+              padding-top: (3 / 2) * 100%;
+            }
+            > .inner {
+              position: absolute;
+              top: 0;
+              right: 0;
+              bottom: 0;
+              left: 0;
+            }
+          }
         }
       }
     }
     @media screen and (max-width: 768px) {
-      padding: 5px 0;
+      padding: 0;
+      .wrapper {
+        .card-wrapper {
+          display: flex;
+          flex-wrap: nowrap;
+          overflow-x: auto;
+          padding: 0;
+          -webkit-overflow-scrolling: touch;
+          .card {
+            flex: 0 0 auto;
+            min-width: 130px;
+            height: auto;
+            margin: 5px;
+          }
+        }
+      }
     }
   }
 }
